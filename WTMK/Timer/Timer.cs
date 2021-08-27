cd8d1e604bcc57f3d433d05dc18a35fae161beb8
Brandon Khan <38743905+wtmk-dev@users.noreply.github.com>
2021-08-27 13:03:03 -0700

    Update Timer.cs

--- WTMK/Timer/Timer.cs
+++ WTMK/Timer/Timer.cs
@@ -16,89 +16,41 @@ public class Timer
             return;
         }
 
-        if (_Timer.ElapsedMilliseconds != _PreviousTimeDiff)
+        if(_Timer.ElapsedMilliseconds >= _TimerLength)
         {
-            _DiffTimeCalc = _Timer.ElapsedMilliseconds - _PreviousTimeDiff;
-            if (_DiffTimeCalc > _TickSpeed)
-            {
-                _PreviousTimeDiff = _Timer.ElapsedMilliseconds;
-                _TimeRemaning -= _TickSpeed;
-
-                OnTimerTick?.Invoke(_TimeRemaning);
-            }
-        }
-
-        if(_TimeRemaning <= 0f)
-        {
-            StopTimer();
+            Stop();
             OnTimerComplete?.Invoke();
         }
     }
 
-    public void StartTimer()
+    public void Start()
     {
         IsTicking = true;
         _Timer.Reset();
         _Timer.Start();
     }
 
-    public void StopTimer()
-    {
-        IsTicking = false;
-        _Timer.Stop();
-    }
-
-    public void CompleteTimer()
-    {
-        IsTicking = false;
-        _Timer.Stop();
-        OnTimerComplete?.Invoke();
-    }
-
-    public void Reset()
-    {
-        IsTicking = false;
-        _TimeRemaning = _TimerLength;
-        _PreviousTimeDiff = 0;
-        _DiffTimeCalc = 0;
-    }
-
-    public void Restart()
-    {
-        Reset();
-        StartTimer();
-    }
-
-    public void Restart(float lenghtOfTimeInMilli, float tickSpeedInMilli)
-    {
-        SetTimer(lenghtOfTimeInMilli);
-        SetTick(tickSpeedInMilli);
-        Reset();
-        StartTimer();
-    }
-
-    public void SetTimer(float lenghtOfTimeInMilli)
+    public void Start(float lenghtOfTimeInMilli)
     {
         _TimerLength = lenghtOfTimeInMilli;
+        IsTicking = true;
+        _Timer.Reset();
+        _Timer.Start();
     }
 
-    public void SetTick(float tickSpeedInMilli)
+    public void Stop()
     {
-        _TickSpeed = tickSpeedInMilli;
+        IsTicking = false;
+        _Timer.Stop();
     }
 
     private System.Diagnostics.Stopwatch _Timer;
     private float _TimerLength;
-    private float _DiffTimeCalc;
-    private float _PreviousTimeDiff;
-    private float _TickSpeed;
-    private float _TimeRemaning;
 
-    public Timer(float lenghtOfTimeInMilli, float tickSpeedInMilli)
+    public Timer(float lenghtOfTimeInMilli)
     {
         _Timer = new System.Diagnostics.Stopwatch();
         _TimerLength = lenghtOfTimeInMilli;
-        _TickSpeed = tickSpeedInMilli;
     }
 
 }
