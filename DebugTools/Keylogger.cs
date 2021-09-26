bcef85ec4acab8c7463e0f15954fce186acb06ae
Brandon Khan <38743905+wtmk-dev@users.noreply.github.com>
2021-09-25 19:23:21 -0700

    Update Keylogger.cs

--- DebugTools/Keylogger.cs
+++ DebugTools/Keylogger.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Keylogger : MonoBehaviour
+public class Keylogger 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
-    void Update()
+    public void OnUpdate()
     {
-        
+        System.Array values = System.Enum.GetValues(typeof(KeyCode));
+        foreach (KeyCode code in values)
+        {
+            if (Input.GetKeyDown(code)) { Debug.Log(System.Enum.GetName(typeof(KeyCode), code)); }
+        }
     }
 }
