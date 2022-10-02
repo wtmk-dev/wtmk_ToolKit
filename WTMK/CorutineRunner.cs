3bc6189e0a808b17728f7fd1be5e07b710cac5c8
Brandon Khan <38743905+wtmk-dev@users.noreply.github.com>
2022-10-02 09:47:35 -0700

    Update CorutineRunner.cs

--- WTMK/CorutineRunner.cs
+++ WTMK/CorutineRunner.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public sealed class CorutineRunner : SingeltonMonoBehavior<CorutineRunner>
 {
+    public IEnumerator Coroutine;
+
     public void Run(IEnumerator routine)
     {
         StartCoroutine(routine);
